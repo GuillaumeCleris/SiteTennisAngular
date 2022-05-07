8d0e10d8cd600d408cdac6819ed7ac85c153862a
Guillaume CLERIS <97674349+GuillaumeCleris@users.noreply.github.com>
2022-05-07 18:38:52 +0200

    generation des pages (ng generate component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TournoisComponent } from './tournois/tournois.component';
+import { ClassementComponent } from './classement/classement.component';
+import { PlanningComponent } from './planning/planning.component';
+import { LeClubComponent } from './le-club/le-club.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TournoisComponent,
+    ClassementComponent,
+    PlanningComponent,
+    LeClubComponent
   ],
   imports: [
     BrowserModule,
