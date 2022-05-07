4100d442efb39f221834be11279b516da989d9b0
Guillaume CLERIS <97674349+GuillaumeCleris@users.noreply.github.com>
2022-05-07 19:35:04 +0200

    Routing (fil d'ariane)
    npm install semantic-ui-css
    routing des pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path:'planning', 
+    loadChildren: () => 
+      import('./planning/planning.module').then((m) => m.PlanningModule) 
+  },
+  { 
+    path:'tournois', 
+    loadChildren: () => 
+      import('./tournois/tournois.module').then((m) => m.TournoisModule) 
+  },
+  { 
+    path:'classement', 
+    loadChildren: () => 
+      import('./classement/classement.module').then((m) => m.ClassementModule) 
+  },
+  { 
+    path:'le-club', 
+    loadChildren: () => 
+      import('./le-club/le-club.module').then((m) => m.LeClubModule) 
+  },
+  { path: '', component: HomeComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { TournoisComponent } from './tournois/tournois.component';
-import { ClassementComponent } from './classement/classement.component';
-import { PlanningComponent } from './planning/planning.component';
-import { LeClubComponent } from './le-club/le-club.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    TournoisComponent,
-    ClassementComponent,
-    PlanningComponent,
-    LeClubComponent
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
