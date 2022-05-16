8ce5173c4632833113973fb60da2ca0aa412ff26
Guillaume CLERIS <97674349+GuillaumeCleris@users.noreply.github.com>
2022-05-16 17:24:21 +0200

    commit oublié

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  signedin$: BehaviorSubject<boolean|null>;
+  constructor(private authService: AuthService) {
+    this.signedin$ = this.authService.signedin$;
+  }
 
   ngOnInit(): void {
   }
 
 }
+
